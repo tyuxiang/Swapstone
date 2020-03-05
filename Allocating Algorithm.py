2ca57526a4639aab57b604087f5d952c47b34435
BobXiao97 <xtq97@163.com>
2020-03-05 19:12:09 +0800

    updated algorithm

--- Allocating Algorithm.py
+++ Allocating Algorithm.py
@@ -21,7 +21,7 @@ class Booth:
         return c
     
     def get_name(self):
-        d=self.name
+        d=self.name 
         return d
     
     def set_position(self,pos):
@@ -36,7 +36,7 @@ booth_list=[]
 booth1=Booth(2,2,'booth1')
 booth2=Booth(3,4,'booth2')
 booth3=Booth(4,5,'booth3')
-booth4=Booth(20,20,'booth4')
+booth4=Booth(3,2,'booth4')
 booth_list.append(booth1)
 booth_list.append(booth2)
 booth_list.append(booth3)
@@ -60,26 +60,35 @@ def find_position(booth_list,space_list):
 
     for m in range(0,len(booth_list)):  #for every Booth in the booth_list
         widt=booth_list[m].width        #get its length and width
-        leng=booth_list[m].length   
-        n=min(space_left)               #the starting point, always start from the smallest
-        checking_list=[]
-        for o in range(0,leng):         #check if all the required spaces are available
-            p=n+o
-            if(p in space_left and math.floor((p-1)/col_num)==math.floor((n-1)/col_num)):
-                checking_list.append(p)
-        for q in range(1,widt):
-            r=n+q*col_num
-            if(r in space_left):
-                checking_list.append(r)
-        if(len(checking_list)==leng+widt-1):    #if it fits in the space left
-            for r in range(leng,leng+widt-1):
-                for s in range(1,leng):
-                    checking_list.append(checking_list[r]+s)
-        if len(checking_list)==booth_list[m].area:   #if all spaces are available, set the position to the booth, remove the space taken from space_left
-            booth_list[m].set_position(n)
-            for t in checking_list:
-                space_left[t-1]=10000
-        result[booth_list[m].name]=booth_list[m].position
+        leng=booth_list[m].length  
+        n=min(space_left)   #the starting point, always start from the smallest
+        x=1
+        while(x==1):    
+            checking_list=[]
+            for o in range(0,leng):         #check if all the required spaces are available
+                p=n+o
+                if(p in space_left and math.floor((p-1)/col_num)==math.floor((n-1)/col_num)):
+                    checking_list.append(p)
+            for q in range(1,widt):
+                r=n+q*col_num
+                if(r in space_left):
+                    checking_list.append(r)
+            if(len(checking_list)==leng+widt-1):    #if it fits in the space left
+                for r in range(leng,leng+widt-1):
+                    for s in range(1,leng):
+                        checking_list.append(checking_list[r]+s)
+            if len(checking_list)==booth_list[m].area:   #if all spaces are available, set the position to the booth, remove the space taken from space_left
+                booth_list[m].set_position(n)
+                for t in checking_list:
+                    space_left[t-1]=10000
+                x=0
+            else:
+                a=space_left.pop(space_left.index(n))
+                if len(space_left)==0:
+                    x=0
+                else:
+                    n=min(space_left)
+            result[booth_list[m].name]=booth_list[m].position
     return result
 
 print(find_position(booth_list,space_list))

826c2775da3fc5961fa9b3d8afec406ef70c0570
tyuxiang <tyuxiang99@gmail.com>
2020-03-05 17:51:16 +0800

    added login page and made main page nicer

--- capstone/views.py
+++ capstone/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'capstone/home.html')
+def login(request):
+	return render(request, 'capstone/login_page.html')

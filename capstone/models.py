cbe0279b4da220775e3a2a5d9a04169a9e8c47d4
unknown <crystalyeohje@gmail.com>
2020-03-06 12:20:48 +0800

    Added models under capstone models

--- capstone/models.py
+++ capstone/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Booth(models.Model):
+    length =  models.DecimalField(max_length=5)
+    width =  models.DecimalField(max_length=5)
+    area =   length*width
+    project_name =  models.CharField(max_length=100)
+    position_x =models.DecimalField(max_length=100)
+    position_y =models.DecimalField(max_length=100)
+    in_campus_centre =models.BooleanField()
+    saved_map = models.ForeignKey(Map,on_delete=models.CASCADE)
+
+
+class Map(models.Model):
+    name = models.CharField(max_length = 100)
+

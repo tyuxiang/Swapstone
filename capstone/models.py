0ab8166c9346501df5dbfff8b8378ba8624cec5d
tyuxiang <tyuxiang99@gmail.com>
2020-03-15 01:07:57 +0800

    added the csv page and made some minor changes to the models in order to run the webpage

--- capstone/models.py
+++ capstone/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Booth(models.Model):
-    length =  models.DecimalField(max_length=5)
-    width =  models.DecimalField(max_length=5)
-    area =   length*width
+    length =  models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
+    width =  models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
+    area =   models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
     project_name =  models.CharField(max_length=100)
-    position_x =models.DecimalField(max_length=100)
-    position_y =models.DecimalField(max_length=100)
+    position_x =models.DecimalField(decimal_places=2,max_digits=10,max_length=100)
+    position_y =models.DecimalField(decimal_places=2,max_digits=10,max_length=100)
     in_campus_centre =models.BooleanField()
-    saved_map = models.ForeignKey(Map,on_delete=models.CASCADE)
+    # saved_map = models.ForeignKey(Map,on_delete=models.CASCADE)
 
 
 class Map(models.Model):

3c54ee5f4d3b4ca3c37e520ffa84a0399abd4706
unknown <crystalyeohje@gmail.com>
2020-03-23 20:37:57 +0800

    integrated allocation algo with database

--- capstone/models.py
+++ capstone/models.py
@@ -7,9 +7,14 @@ class Booth(models.Model):
     width =  models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
     area =   models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
     project_name =  models.CharField(max_length=100)
-    position_x =models.DecimalField(decimal_places=2,max_digits=10,max_length=100,null=True)
-    position_y =models.DecimalField(decimal_places=2,max_digits=10,max_length=100,null=True)
+    industry =  models.CharField(max_length=100)
+    length_pixel =  models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
+    width_pixel =  models.DecimalField(decimal_places=2,max_digits=10,max_length=5)
+    rotation = models.DecimalField(decimal_places=2,max_digits=10,max_length=5,default = -1)
+    position_x =models.DecimalField(decimal_places=2,max_digits=10,max_length=100,default = -1)
+    position_y =models.DecimalField(decimal_places=2,max_digits=10,max_length=100,default =-1)
     in_campus_centre =models.BooleanField()
+    
     # saved_map = models.ForeignKey(Map,on_delete=models.CASCADE)
 
 
--- capstone/views.py
+++ capstone/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from .load_csv_data import load_csv_data
-from capstone.models import Booth
+from .allocation import allocate
+
 
 # Create your views here.
 def home(request):
@@ -18,6 +19,7 @@ def csv(request):
 		filename = fs.save(myfile.name, myfile)
 		uploaded_file_url =filename
 		load_csv_data(uploaded_file_url)
+		allocate()
 		return render(request, 'capstone/csv.html')
 	return render(request, 'capstone/csv.html')
 	#return render(request, 'capstone/csv.html')

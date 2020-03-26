fa2c3b3a1084972a6249ad2658da3b94f985067a
tyuxiang <tyuxiang99@gmail.com>
2020-03-26 12:43:19 +0800

    test

--- capstone/tests.py
+++ capstone/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from .load_csv_data import load_csv_data
+from django.contrib.auth.models import User
 # Create your tests here.
 
 
@@ -7,4 +8,12 @@ class UploadTest(TestCase):
     def test_upload_correct(self):
         client = Client()
         client.post('/')
+        User.objects.create_user('crystal',password='yeohje00')
         self.assertTrue(client.login(username = 'crystal', password = 'yeohje00'))
+
+    def load_csv(self):
+        c = Client()
+        User.objects.create_user('crystal',password='yeohje00')
+        c.login(username = 'crystal', password = 'yeohje00')
+        with open('../../somedata.xlsx') as fp:
+            c.post('/load_csv', {'input': fp})

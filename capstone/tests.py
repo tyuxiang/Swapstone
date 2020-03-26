acab7767268c8acd7858040bcdd64b12a9557bd6
unknown <hansingjunitra@outlook.com>
2020-03-26 13:01:29 +0800

    test selenium hansing

--- capstone/tests.py
+++ capstone/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+from django.contrib.auth.models import User
+import time
 
-# Create your tests here.
+class MySeleniumTests(StaticLiveServerTestCase):
+    # fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+        User.objects.create_user(username="crystal",password="yeohje00")
+        super(MySeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('yeohje00')
+        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
+        # time.sleep(3)
+        time.sleep(10)
+        map_layout = self.selenium.find_element_by_name("map-layout")
+        actions = ActionChains(self.selenium)
+        actions.move_to_element(map_layout)
+        actions.double_click()
+        actions.perform()
+        self.assertEqual(response.status_code, 200)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('secret')
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()# Create your tests here.
+
+    

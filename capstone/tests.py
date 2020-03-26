f03168caddfc71901b46c8e9ada963b7cc7ecf18
unknown <crystalyeohje@gmail.com>
2020-03-26 13:44:20 +0800

    hi

--- capstone/tests.py
+++ capstone/tests.py
@@ -3,6 +3,7 @@ from .load_csv_data import load_csv_data
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 from django.contrib.auth.models import User
+import time
 # Create your tests here.
 
 
@@ -12,23 +13,10 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
-        
-
-
     
-
-    def wrong_username(self):
-
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('crystal')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('yeohje11')
-        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
-        # message = self.selenium.find_element_by_xpath('/html/body/div/form/p')
-        # assert message =="Your username and password didn't match. Please try again."
-
     
+    # def test_account_creation(self):
+
 
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/'))
@@ -40,8 +28,12 @@ class MySeleniumTests(StaticLiveServerTestCase):
         password_input.send_keys('yeohje00')
         second_password_input = self.selenium.find_element_by_name("password")
         second_password_input.send_keys('yeohje00')
+        url = self.selenium.get(self.live_server_url)
+        print(url)
         username_input = self.selenium.find_element_by_xpath("/html/body/div/form/div[1]/button").click()
         print("hi")
+    
+
         self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys('crystal')
@@ -49,6 +41,18 @@ class MySeleniumTests(StaticLiveServerTestCase):
         password_input.send_keys('yeohje00')
         self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
         #self.selenium.find_element_by_xpath('/html/body/nav/a')
-        url = self.browser.current_url
-        
-        
+        url = self.selenium.get(self.live_server_url)
+        print(url)
+
+    def wrong_username(self):
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('yeohje00')
+        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
+        message = self.selenium.find_element_by_xpath('/html/body/div/form/p')
+        assert message.text =="Your username and password didn't match. Please try again."
+
+    

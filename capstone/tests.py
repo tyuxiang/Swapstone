1df431e2a117f7044353382060095e1a45767223
unknown <crystalyeohje@gmail.com>
2020-03-26 13:25:54 +0800

    CANN LOGINNNN

--- capstone/tests.py
+++ capstone/tests.py
@@ -12,28 +12,43 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
-        User.objects.create_user("crystal","yeohje00")
-    
-    def test_login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('crystal')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('yeohje00')
-        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
-        self.selenium.find_element_by_xpath('/html/body/nav/a').click()
-        
         
 
+
+    
 
     def wrong_username(self):
+
         self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys('crystal')
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys('yeohje11')
         self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
-        message = self.selenium.find_element_by_xpath('/html/body/div/form/p')
-        assert message =="Your username and password didn't match. Please try again."
+        # message = self.selenium.find_element_by_xpath('/html/body/div/form/p')
+        # assert message =="Your username and password didn't match. Please try again."
 
     
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        
+        username_input = self.selenium.find_element_by_xpath("/html/body/div/form/div[4]/a").click()
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = self.selenium.find_element_by_name("first_password")
+        password_input.send_keys('yeohje00')
+        second_password_input = self.selenium.find_element_by_name("password")
+        second_password_input.send_keys('yeohje00')
+        username_input = self.selenium.find_element_by_xpath("/html/body/div/form/div[1]/button").click()
+        print("hi")
+        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('yeohje00')
+        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
+        #self.selenium.find_element_by_xpath('/html/body/nav/a')
+        url = self.browser.current_url
+        
+        

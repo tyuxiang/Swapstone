6fb2773e2e6944931fec2e7ded0e814772a900b9
unknown <hansingjunitra@outlook.com>
2020-03-29 02:49:29 +0800

    main page features test case added

--- capstone/tests.py
+++ capstone/tests.py
@@ -13,6 +13,23 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
+        cls.selenium.get('%s%s' % (cls.live_server_url, '/'))
+        username_input = cls.selenium.find_element_by_xpath("/html/body/div/form/div[4]/a").click()
+        username_input = cls.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = cls.selenium.find_element_by_name("first_password")
+        password_input.send_keys('yeohje00')
+        second_password_input = cls.selenium.find_element_by_name("password")
+        second_password_input.send_keys('yeohje00')
+        username_input = cls.selenium.find_element_by_xpath("/html/body/div/form/div[1]/button").click()
+        cls.selenium.get('%s%s' % (cls.live_server_url, '/accounts/login/'))
+        username_input = cls.selenium.find_element_by_name("username")
+        username_input.send_keys('crystal')
+        password_input = cls.selenium.find_element_by_name("password")
+        password_input.send_keys('yeohje00')
+        cls.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
+        time.sleep(1)
+        cls.selenium.refresh()
         
     # def wrong_username(self):
 
@@ -60,48 +77,67 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.selenium.refresh()
 
     def test_zoom(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        
-        username_input = self.selenium.find_element_by_xpath("/html/body/div/form/div[4]/a").click()
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('crystal')
-        password_input = self.selenium.find_element_by_name("first_password")
-        password_input.send_keys('yeohje00')
-        second_password_input = self.selenium.find_element_by_name("password")
-        second_password_input.send_keys('yeohje00')
-        username_input = self.selenium.find_element_by_xpath("/html/body/div/form/div[1]/button").click()
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('crystal')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('yeohje00')
-        self.selenium.find_element_by_xpath('/html/body/div/form/div[2]/button').click()
-        time.sleep(1)
-        self.selenium.refresh()
-        # time.sleep(5)
-        # self.selenium.find_element_by_xpath('/html/body/div/div/nav[1]/div/ul[1]/li[2]/a').click()
-        # time.sleep(1)
-        # self.selenium.find_element_by_xpath('/html/body/div/div/main/div/div/form/button').click()
-        # self.selenium.find_element_by_name('input').click()
-        #url = self.browser.current_url
         map_layout = self.selenium.find_element_by_xpath('//*[@id="map-layout"]')
         actions = ActionChains(self.selenium)
         actions.move_to_element(map_layout)
         actions.double_click()
         actions.perform()
-        # scale = map_layout.get_attribute("transform")
-        # if (scale != '2'):
-        #     print("Zoom Test Case 1 - Before Clicking Zoom and Pan Button: TRUE")
-        # else:
-        #     print("Zoom Test Case 1 - before Clicking Zoom and Pan Button: FALSE")
-    
+        scale = map_layout.get_attribute("transform").split(' ')[1][6:-1]
+        self.assertEqual(scale, '1')
         self.selenium.find_element_by_xpath('//*[@id="zoom-button"]').click()
         actions.move_to_element(map_layout)
         actions.double_click()
         actions.perform()
         scale = map_layout.get_attribute("transform").split(' ')[1][6:-1]
-        if (scale == '2'):
-            print("Zoom Test Case 2 - After Clicking Zoom and Pan Button: TRUE")
-        else:
-            print("Zoom Test Case 2 - After Clicking Zoom and Pan Button: FALSE")
-        
+        self.assertNotEqual(scale, '1')
+
+    def test_drag(self):
+        booth_test = self.selenium.find_element_by_xpath('//*[@id="booth-id-1"]')
+        initial_x_offset = booth_test.get_attribute('x')
+        initial_y_offset = booth_test.get_attribute('y')
+        actions = ActionChains(self.selenium)
+        actions.drag_and_drop_by_offset(booth_test, '400', '400')
+        final_x_offset = booth_test.get_attribute('x')
+        final_y_offset = booth_test.get_attribute('y')
+        self.assertEqual(initial_x_offset, final_x_offset)
+        self.assertEqual(initial_y_offset, final_y_offset)
+        self.selenium.find_element_by_xpath('//*[@id="dragbooth-button"]').click()
+        actions = ActionChains(self.selenium)
+        actions.drag_and_drop_by_offset(booth_test, '400', '400')
+        final_x_offset = booth_test.get_attribute('x')
+        final_y_offset = booth_test.get_attribute('y')
+        self.assertNotEqual(initial_x_offset, final_x_offset)
+        self.assertNotEqual(initial_y_offset, final_y_offset)
+        self.assertEqual(final_x_offset, '400')
+        self.assertEqual(final_y_offset, '400')
+
+    def test_x_input(self):
+        booth_test = self.selenium.find_element_by_xpath('//*[@id="booth-id-1"]')
+        x_input = self.selenium.find_element_by_name('input-x')
+        x_input.send_keys('600')
+        x_offset = booth_test.get_attribute('x')
+        self.assertEqual(x_offset, '600')
+
+    def test_y_input(self):
+        booth_test = self.selenium.find_element_by_xpath('//*[@id="booth-id-1"]')
+        y_input = self.selenium.find_element_by_name('input-y')
+        y_input.send_keys('600')
+        y_offset = booth_test.get_attribute('y')
+        self.assertEqual(y_offset, '600')
+
+
+    def test_length_input(self):
+        booth_test = self.selenium.find_element_by_xpath('//*[@id="booth-id-1"]')
+        length_input = self.selenium.find_element_by_name('input-length')
+        length_input.send_keys('3')
+        length_offset = booth_test.get_attribute('height')
+        self.assertEqual(length_offset, '40.191')
+
+    def test_width_input(self):
+        booth_test = self.selenium.find_element_by_xpath('//*[@id="booth-id-1"]')
+        width_input = self.selenium.find_element_by_name('input-width')
+        width_input.send_keys('4')
+        width_offset = booth_test.get_attribute('width')
+        self.assertEqual(width_offset, '53.588')
+
+# <rect class="booth" id="booth-id-1" x="465.14" y="484.56999999999994" height="28.57" width="28.57" clip-path="url(#clip)" transform="rotate(0 479.425 499.85499999999996)"></rect>
